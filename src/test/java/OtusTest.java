270ad947b90ae4772472f1f9174cd92459a8b36b
NobodyThe1 <117182134+NobodyThe1@users.noreply.github.com>
2023-01-02 21:51:04 +0300

    Update OtusTest.java

--- src/test/java/OtusTest.java
+++ src/test/java/OtusTest.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
